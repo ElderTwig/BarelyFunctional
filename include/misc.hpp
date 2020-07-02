f72237c95e234da496f8c135a1e5bedf6700245d
ElderTwig <pqfrml@gmail.com>
2020-07-02 17:04:08 +0200

    Add Concat and Concat_t

--- include/misc.hpp
+++ include/misc.hpp
@@ -1,4 +1,32 @@
 #ifndef BARELYFUNCTIONAL_MISC_HPP
 #define BARELYFUNCTIONAL_MISC_HPP
 
+#include <type_traits>
+#include <variant>
+
+namespace BarelyFunctional {
+
+template<class...>
+struct Concat {};
+
+template<template<class...> class Compound, class... Us>
+struct Concat<Compound<Us...>> {
+    using type = Compound<Us...>;
+};
+
+template<
+        template<class...>
+        class Compound,
+        class... Us,
+        class... Ts,
+        class... Compounds>
+struct Concat<Compound<Us...>, Compound<Ts...>, Compounds...> {
+    using type = typename Concat<Compound<Us..., Ts...>, Compounds...>::type;
+};
+
+template<class... Compounds>
+using Concat_t = typename Concat<Compounds...>::type;
+
+}    // namespace BarelyFunctional
+
 #endif    // BARELYFUNCTIONAL_MISC_HPP
--- tests/misc_tests.hpp
+++ tests/misc_tests.hpp
@@ -3,4 +3,33 @@
 
 #include "misc.hpp"
 
+#include <catch2/catch.hpp>
+
+TEST_CASE("Concat concattenates compound types", "[static][Concat][Concat_t]")
+{
+    SECTION("std::variant")
+    {
+        using var1 = std::variant<int, double>;
+        using var2 = std::variant<float, char>;
+
+        using result = BarelyFunctional::Concat_t<var1, var2, var1>;
+
+        STATIC_REQUIRE(std::is_same_v<
+                       result,
+                       std::variant<int, double, float, char, int, double>>);
+    }
+
+    SECTION("std::tuple")
+    {
+        using var1 = std::tuple<int, double>;
+        using var2 = std::tuple<float, char>;
+
+        using result = BarelyFunctional::Concat_t<var1, var2, var1>;
+
+        STATIC_REQUIRE(std::is_same_v<
+                       result,
+                       std::tuple<int, double, float, char, int, double>>);
+    }
+}
+
 #endif    // BARELYFUNCTIONAL_MISC_TESTS_HPP
