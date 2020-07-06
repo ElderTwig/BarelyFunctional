01994602157f1685dde6d05a2aa3a70e079ef375
ElderTwig <pqfrml@gmail.com>
2020-07-07 00:50:58 +0200

    Add free function wrapper

---
+++ include/wrap.hpp
@@ -0,0 +1,10 @@
+
+#ifndef BARELYFUNCTIONAL_WRAP_HPP
+#define BARELYFUNCTIONAL_WRAP_HPP
+
+#define BARELY_WRAP(functionName)                                   \
+    [](auto&&... args) {                                            \
+        return functionName(std::forward<decltype(args)>(args...)); \
+    }
+
+#endif    // BARELYFUNCTIONAL_WRAP_HPP
